c73db6c17d35fd7a7f42a1e500fbc478801e3797
Brian Jacobs <brian5jacobs@hotmail.com>
2025-08-27 16:55:57 +0200

    Last module complete

--- module04/ex02/callLimit.py
+++ module04/ex02/callLimit.py
@@ -1,13 +1,12 @@
 def callLimit(limit: int):
     count = 0
     def callLimiter(function):
-        nonlocal count
-        count += 1
         def limit_function(*args, **kwds):
-            nonlocal function
+            nonlocal count
+            count += 1
 
             if count <= limit:
                 return function(*args, **kwds)
             print(f"Error: {function} called too many times")
-
+        return limit_function
     return (callLimiter)
